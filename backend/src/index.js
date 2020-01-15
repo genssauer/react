8a062e117fc7070e8fb954f77f3934d9a82f2179
genesson <genesson_sauer@hotmail.com>
2020-01-14 22:17:09 -0300

    feat(*): update index js

---
+++ backend/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-k9mj1.mongodb.net/wee10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
